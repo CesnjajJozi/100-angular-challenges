dcf56a30b76a33a6aa75e9f3580bcdc60106d1e3
jozi <cesnjaj.jozi@gmail.com>
2025-08-08 21:36:03 +0200

    Dpendencies upgrade, stlye/ts error fixes

--- src/app/directives/debounce-click/debounce-click.directive.ts
+++ src/app/directives/debounce-click/debounce-click.directive.ts
@@ -1,13 +1,18 @@
-import { Directive, Output, EventEmitter, OnInit, OnDestroy, HostListener } from '@angular/core';
-import { Subject, Subscription } from 'rxjs';
-import { debounceTime } from 'rxjs/internal/operators/debounceTime';
+import {
+  Directive,
+  EventEmitter,
+  HostListener,
+  OnDestroy,
+  OnInit,
+  Output,
+} from "@angular/core";
+import { Subject, Subscription } from "rxjs";
+import { debounceTime } from "rxjs/internal/operators/debounceTime";
 
-@Directive(
-  {
-    selector: '[appDebounceClick]',
-    standalone: false
-}
-)
+@Directive({
+  selector: "[appDebounceClick]",
+  standalone: false,
+})
 export class DebounceClickDirective implements OnInit, OnDestroy {
   @Output() public debounceClick = new EventEmitter();
   public clicks = new Subject();
@@ -16,20 +21,19 @@ export class DebounceClickDirective implements OnInit, OnDestroy {
   public ngOnInit(): void {
     const debounceTimeInMs = 500;
 
-    this.subscription = this.clicks.pipe(
-      debounceTime(debounceTimeInMs)
-    ).subscribe((value: any) => this.debounceClick.emit(value));
+    this.subscription = this.clicks
+      .pipe(debounceTime(debounceTimeInMs))
+      .subscribe((value: any) => this.debounceClick.emit(value));
   }
 
   public ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
 
-  @HostListener('click', ['$event'])
+  @HostListener("click", ["$event"])
   public clickEvent(event: any): void {
     event.preventDefault();
     event.stopPropagation();
-    this.clicks.next();
+    this.clicks.next(event);
   }
-
 }
